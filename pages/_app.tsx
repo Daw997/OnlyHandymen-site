d0e10cb5c2d083ade1ebe3089d88a0d87d16d0b1
Daw997 <dawidek.street15@gmail.com>
2025-04-04 22:50:13 +0200

    Create _app.tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,15 @@
+import '../styles/globals.css';
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
+import { useState } from 'react';
+import type { AppProps } from 'next/app';
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+
+  return (
+    <SessionContextProvider supabaseClient={supabaseClient} initialSession={pageProps.initialSession}>
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  );
+}
